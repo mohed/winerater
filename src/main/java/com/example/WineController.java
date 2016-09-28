a125d8e594598d9bfbc97c5eb3ce23fa1a54729b
Roberto <skinkpajen@gmail.com>
2016-09-28 15:40:58 +0200

    updated results.html

---
+++ src/main/java/com/example/WineController.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by Roberto Angius on 2016-09-28.
+ */
+
+@RestController
+public class WineController {
+
+    @GetMapping("/results")
+    public ModelAndView matchingWines(){
+
+        return new ModelAndView("results");
+    }
+
+}
