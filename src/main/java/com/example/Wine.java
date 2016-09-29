59964d28d9ed5957ae470a58924cf5e97fbec867
Ella Halden <ella.halden@live.se>
2016-09-29 11:55:45 +0200

    Update query branch

--- src/main/java/com/example/Wine.java
+++ src/main/java/com/example/Wine.java
@@ -4,27 +4,11 @@ package com.example;
  * Created by Administrator on 2016-09-28.
  */
 public class Wine {
-    private String name;
-    private int artnum;
+    public final String name;
+    public final long artnum;
 
-    public Wine(String name, int artnum) {
+    public Wine(long artnum, String name) {
         this.name = name;
-        this.artnum = artnum;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getArtnum() {
-        return artnum;
-    }
-
-    public void setArtnum(int artnum) {
         this.artnum = artnum;
     }
 }
