cb7fda2a3c91febdae850e5c374ea27a41305e67
Roberto <skinkpajen@gmail.com>
2016-09-30 11:46:19 +0200

    CSS IS WORKING 3.0

--- src/main/java/com/example/WineraterApplication.java
+++ src/main/java/com/example/WineraterApplication.java
@@ -2,14 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @SpringBootApplication
