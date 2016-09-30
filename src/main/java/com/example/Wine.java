1e7da7aa7bb622a94597c6a85e096d4025661ee9
Ella Halden <ella.halden@live.se>
2016-09-30 11:00:05 +0200

    User + Wine + rating update

--- src/main/java/com/example/Wine.java
+++ src/main/java/com/example/Wine.java
@@ -8,12 +8,14 @@ public class Wine {
     public final int artnum;
     public final int year;
     public final int price;
+    public final int rating;
 
 
-    public Wine(String name, int artnum, int year, int price) {
+    public Wine(String name, int artnum, int year, int price, int rating) {
         this.name = name;
         this.artnum = artnum;
         this.year = year;
         this.price = price;
+        this.rating = rating;
     }
 }
